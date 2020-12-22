392334d2b9d6881d5b6f8c534c9e389cc8ec0862
karimelshaarawy <kemogogo69@gmail.com>
2020-12-22 21:11:16 +0200

    changed some book names and fixed book repeatition

--- main/controllers/addBookController.js
+++ main/controllers/addBookController.js
@@ -1,15 +1,17 @@
 var fs = require('fs');
+var path = require('path');
+
 
 function addBook(req, res) {
     const book = req.body.bookName;
     var user = req.cookies;
     var username = user.username;
-    console.log(username);
     const data = fs.readFileSync(path.join('base.json'));
     const users = JSON.parse(data);
     const array = users.array;
     for (var i = 0; i < array.length; i++) {
         if (array[i].username === username)
+         if(!array[i].books.includes(book))
             array[i].books.push(book);
     }
     var newusers = JSON.stringify(users);
